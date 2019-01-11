3ac94eebf1303e3ce64903ab9049b40abc47e6bf
王强 <bodysouls@tom.com>
2019-01-11 11:56:10 +0800

    Initial commit

---
+++ src/main/java/day02/KeyMap.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class KeyMap {
+    // psvm 主方法
+    public static void main(String[] args) {
+        // sout 打印语句
+        System.out.println("快捷键的学习");
+        // c + s +ent 跳到编辑区
+        // alt + num 跳转窗口
+        // c + s +f10 运行
+        //
+    }
+}
